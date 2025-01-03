fb391bce819ebf82a24c650a8891c5f58a31d932
Seema-476 <simmybishnoi29085@gmail.com>
2025-01-03 15:33:21 +0530

    set the form btn

--- src/components/EventsEinsenden.jsx
+++ src/components/EventsEinsenden.jsx
@@ -46,7 +46,7 @@ const EventsEinsenden = () => {
                 <Heading text="Event einsenden" />
                 <p className='font-light sm:text-xl text-base sm:leading-custom-2xl text-gray text-center mx-auto max-w-[768px] py-5'>At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
                 <form onSubmit={handleSubmit}>
-                    <div className='bg-white rounded-[30px] p-4 max-w-[763px] mx-auto mb-5 border-light-blue border-[0.5px] border-dashed'>
+                    <div className='bg-white rounded-[30px] p-4 max-w-[763px] mx-auto md:mb-5 mb-3 border-light-blue border-[0.5px] border-dashed'>
                         <div className='w-24 h-24 flex items-center justify-center border-light-gray border border-dashed rounded-[20px]'>
                             {selectedImages.map((image, index) => (
                                 <img key={index} className="rounded-[20px] max-w-[96px]" src={image} alt={`Selected Image ${index + 1}`} />))}
@@ -76,7 +76,7 @@ const EventsEinsenden = () => {
                             placeholder='Titel'
                             required='required'   
                             value={formData.title} onChange={handleChange}
-                            className="font-bold text-sm leading-custom-xl text-gray mb-5 w-full mx-auto flex py-[17.5px] px-4 rounded-[30px] placeholder:text-gray outline-none border-light-blue border-[0.5px]"
+                            className="font-bold text-sm leading-custom-xl text-gray md:mb-5 mb-3 w-full mx-auto flex py-[17.5px] px-4 rounded-[30px] placeholder:text-gray outline-none border-light-blue border-[0.5px]"
                         />
                         <input
                             type="text"
@@ -85,7 +85,7 @@ const EventsEinsenden = () => {
                             required='required' 
                             value={formData.date}
                             onChange={handleChange}
-                            className="font-bold text-sm leading-custom-xl w-full text-gray mb-5 mx-auto flex py-[17.5px] px-4 rounded-[30px] placeholder:text-gray outline-none border-light-blue border-[0.5px]"
+                            className="font-bold text-sm leading-custom-xl w-full text-gray md:mb-5 mb-3 mx-auto flex py-[17.5px] px-4 rounded-[30px] placeholder:text-gray outline-none border-light-blue border-[0.5px]"
                         />
                     </div>
                     <div className='max-w-[763px] w-full flex max-sm:flex-col mx-auto sm:gap-5'>
@@ -95,7 +95,7 @@ const EventsEinsenden = () => {
                             required='required' 
                             placeholder='Standort'
                             value={formData.location} onChange={handleChange}
-                            className="font-bold text-sm leading-custom-xl text-gray mb-5 w-full mx-auto flex py-[17.5px] px-4 rounded-[30px] placeholder:text-gray outline-none border-light-blue border-[0.5px]"
+                            className="font-bold text-sm leading-custom-xl text-gray md:mb-5 mb-3 w-full mx-auto flex py-[17.5px] px-4 rounded-[30px] placeholder:text-gray outline-none border-light-blue border-[0.5px]"
                         />
                         <input
                             type="text"
@@ -103,7 +103,7 @@ const EventsEinsenden = () => {
                             placeholder='Preis'
                             required='required' 
                             value={formData.price} onChange={handleChange}
-                            className="font-bold text-sm leading-custom-xl w-full text-gray mb-5 mx-auto flex py-[17.5px] px-4 rounded-[30px] placeholder:text-gray outline-none border-light-blue border-[0.5px]"
+                            className="font-bold text-sm leading-custom-xl w-full text-gray md:mb-5 mb-3 mx-auto flex py-[17.5px] px-4 rounded-[30px] placeholder:text-gray outline-none border-light-blue border-[0.5px]"
                         />
                     </div>
                     <textarea
@@ -112,9 +112,9 @@ const EventsEinsenden = () => {
                         name='details'
                         required='required' 
                         value={formData.details} onChange={handleChange}
-                        className="font-bold text-sm leading-custom-xl w-full text-gray mb-5 mx-auto flex pt-[55.5px] px-4 rounded-[30px] max-w-[763px] placeholder:text-gray outline-none border-light-blue border-[0.5px] resize-none"></textarea>
+                        className="font-bold text-sm leading-custom-xl w-full text-gray md:mb-5 mb-3 mx-auto flex pt-[55.5px] px-4 rounded-[30px] max-w-[763px] placeholder:text-gray outline-none border-light-blue border-[0.5px] resize-none"></textarea>
                     <div className='mx-auto text-center'>
-                        <button className='font-bold text-base text-white py-4 px-[115.5px] bg-gradient-to-tr from-[#0071BB] to-[#39A3E7] hover:from-[#39A3E7] hover:to-[#0071BB] duration-1000 rounded-[30px]'>Event einsenden</button>
+                        <button className='font-bold text-base text-white py-4 sm:px-[115.5px] px-24 whitespace-nowrap bg-gradient-to-tr from-[#0071BB] to-[#39A3E7] hover:from-[#39A3E7] hover:to-[#0071BB] duration-1000 rounded-[30px]'>Event einsenden</button>
                     </div>
                 </form>
             </div>
